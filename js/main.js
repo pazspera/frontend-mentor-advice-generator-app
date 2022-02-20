8f9f890ad7885e3ac3125a668640985648de56d2
Paz Spera <mspera03@gmail.com>
2022-02-19 22:50:01 -0300

    corrected getAdvice()

--- js/main.js
+++ js/main.js
@@ -12,16 +12,15 @@
 console.log("hello");
 /* ------ VARIABLES ------ */
 const d = document;
-const URL_ADVICE = "https://api.adviceslip.com/advice/";
+const URL_ADVICE = "https://api.adviceslip.com/advice";
+let $adviceNumber = d.querySelector(".advice-card__number");
+let $adviceText = d.querySelector("advice-card__text");
 
 /* ------ FUNCTIONS ------ */
 const getAdvice = async () => {
 	try {
 		let res = await fetch(URL_ADVICE, {
 			method: "GET",
-			headers: {
-				"Content-type": "text/html; charset=UTF-8",
-			},
 		});
 		let data = await res.json();
 		console.log(data);
@@ -32,6 +31,8 @@ const getAdvice = async () => {
 				statusText: res.statusText,
 			};
 		}
+		console.log(data.id);
+		// $adviceNumber.textContent = `Advice #${slip.id}`;
 	} catch (err) {
 		let message = err.statusText || "An error occurred";
 		console.log(message);
@@ -39,4 +40,4 @@ const getAdvice = async () => {
 };
 
 /* ------ CODE ------ */
-d.addEventListener("DOMContentloaded", getAdvice);
+d.addEventListener("DOMContentloaded", getAdvice());
